8cbd0d0e7e81cbbe2ca9fec24634708505c2c9b1
Adnan-Zhaikh <adnanibrahimshaikh@gmail.com>
2026-09-19 23:33:30 +0530

    POST endpoint

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-from fastapi import FASTAPI
+from fastapi import FastAPI
+from database import init_db, add_checkin
 
+app = FastAPI()
+
+
+@app.post("/checkin")
+def checkin(note: str):
+    add_checkin(note)
+    return {"status": "checked in"}
