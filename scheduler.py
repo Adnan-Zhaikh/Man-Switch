95e762c97643a2058006a9185526cc8b52b0792b
Adnan-Zhaikh <adnanibrahimshaikh@gmail.com>
2026-09-22 19:09:15 +0530

    SOlved the bug in timezone

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,5 @@
 from database import get_last_checkin
-from datetime import datetime
+from datetime import datetime, timezone
 import requests
 from dotenv import load_dotenv; import os; load_dotenv(); NTFY_TOPIC = os.getenv("NTFY_TOPIC")
 
@@ -7,14 +7,11 @@ DEADLINE_HOURS = 24
 
 def check_deadline():
     last = get_last_checkin()
-
     if last is None:
         return False
 
     last_dt = datetime.fromisoformat(last)
-
-    elapsed_hours = (datetime.now() - last_dt).total_seconds() / 3600
-
+    elapsed_hours = (datetime.now(timezone.utc) - last_dt).total_seconds() / 3600
     return elapsed_hours > DEADLINE_HOURS
 
 def send_alert():
